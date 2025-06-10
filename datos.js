0c829679cdd34e4d117f96860152ee6c8c4462ea
Alex09121995 <158055482+Alex09121995@users.noreply.github.com>
2025-06-09 21:05:21 -0400

    primer commit

---
+++ datos.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors'); 
+const Port = 3000
+app.use(cors());
+app.use(bodyParser.json())
+
+app.get('/', (req,res)=>{
+    res.send('hola estoy en linea')
+})
+
+app.listen(Port, ()=>{
+    console.log(`servido escuchando en el puerto: ${Port}`)
+})
