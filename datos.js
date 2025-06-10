61e7d0aa047463231ad0d4f7b7647aa7e777baab
Jesús Hernández <jesushdez1303@gmail.com>
2025-06-09 21:17:58 -0400

    someChanges

--- datos.js
+++ datos.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors'); 
-const Port = 3000
+const Port = 3005
 app.use(cors());
 app.use(bodyParser.json())
 
